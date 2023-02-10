e80bbb00c1f5a96f0b34de150e8db4ed43deef2f
Matheus Figueiredo <mfigueiredoeng@gmail.com>
2023-02-10 10:19:06 -0300

    Criou classe Position

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p;
+
+            
+        }
+    }
+}
