abd7e587e0200435367e50c460b47edec7584d99
Matheus Figueiredo <mfigueiredoeng@gmail.com>
2023-02-13 09:46:41 -0300

    Mudando cor da peça preta

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -13,7 +13,8 @@ namespace Xadrez_Console
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 7));
+                tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(1, 4));
 
                 Tela.imprimirTabuleiro(tab);
 
